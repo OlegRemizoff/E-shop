f4b2d740012eb5e237ca136f702e3ee4601ede1d
Oleg Remizov <job.rem1zoff@gmail.com>
2023-05-24 13:58:58 +0300

    upgrade cart change icon

--- src/cart/context_processors.py
+++ src/cart/context_processors.py
@@ -2,8 +2,13 @@
 
 def cart(request):
     cart = request.session.get('cart')
+    url_array = [] # если slug в корзине будет == item.slug вкл нужные include.html c иконками корзины 
     if cart != None:
         product_count = len(cart)
+        for i in cart: 
+            url_array.append(i['slug'])
+        
     else:
         product_count = 0
-    return {'product_count': product_count}
+    return {'product_count': product_count,'url_array': url_array}
+
