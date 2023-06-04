77ffebb133321c0806bb8a5cc3b98185e42b80c7
Oleg Remizov <job.rem1zoff@gmail.com>
2023-06-04 18:00:36 +0300

    temp change language

--- src/shop/templatetags/mytags.py
+++ src/shop/templatetags/mytags.py
@@ -8,3 +8,10 @@ def get_categories():
     return Category.objects.all()
 
 
+# Функция принимает текущий url адрес и обрасывает язык (/ru/ или /en/)
+# результат будет подставлен в форму в виде href="/lang/{{ res }}" 
+# чтобы при смене языка остаться на той же странице
+@register.simple_tag()
+def get_path_for_lang(path:str)->str:
+    result = path[4:]
+    return result
